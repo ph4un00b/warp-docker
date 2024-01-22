2b333f669f8f3f10e41282a9d55f1d9d628b1010
ph4u <1057021+ph4un00b@users.noreply.github.com>
2024-01-22 16:38:03 -0600

    wip: ping

---
+++ examples/ping.rs
@@ -0,0 +1,25 @@
+#[tokio::main]
+async fn main() {
+    let url = "https://examplejamon.com";
+    let max_retries = 5;
+    let client = reqwest::Client::new();
+
+    for retry in 1..=max_retries {
+        match client.get(url).send().await {
+            Ok(response) if response.status().is_success() => {
+                println!("Service is available!");
+                break;
+            }
+            Ok(response) => println!(
+                "Attempt {} failed with status code: {}",
+                retry,
+                response.status()
+            ),
+            Err(err) => println!("❌ Attempt {} failed with error: {}", retry, err),
+        }
+
+        if retry < max_retries {
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        }
+    }
+}
