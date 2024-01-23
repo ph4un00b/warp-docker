0d757c3ce2dc2cd89defe9eb02133b129bf1d913
ph4u <1057021+ph4un00b@users.noreply.github.com>
2024-01-22 20:24:38 -0600

    wip: ping with backon

---
+++ examples/ping_backon.rs
@@ -0,0 +1,21 @@
+use backon::Retryable;
+
+async fn fetch() -> anyhow::Result<String> {
+    let response = reqwest::get("https://httpbingo.org/unstable?failure_rate=0.9").await?;
+    if !response.status().is_success() {
+        println!("{}", response.status());
+        anyhow::bail!("some kind of error");
+    }
+
+    Ok(response.text().await?)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    fetch
+        .retry(&backon::ExponentialBuilder::default().with_max_times(5))
+        .await?;
+    println!("✅ fetch succeeded");
+
+    Ok(())
+}
